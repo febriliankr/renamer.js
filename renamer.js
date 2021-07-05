73b6613c86609198844f55ea62d584545c11fa0f
Febrilian Kristiawan <febriliankr@gmail.com>
2021-07-05 09:33:08 +0700

    edited configuration for filename

--- renamer.js
+++ renamer.js
@@ -10,38 +10,51 @@ var path = require('path');
 var ANSI_GREEN = '\x1b[32m';
 var ANSI_DEFAULT = '\x1b[0m';
 var ANSI_RED = '\x1b[35m';
-const originFileExtension = "js"
-const newFileExtension = "tsx"
+const originFileExtension = 'ts';
+const newFileExtension = 'tsx';
 
-var walkSync = function(dir, filelist) {
+var walkSync = function (dir, filelist) {
   var files = fs.readdirSync(dir);
   filelist = filelist || [];
-  files.forEach(function(file) {
+  files.forEach(function (file) {
     if (fs.statSync(path.join(dir, file)).isDirectory()) {
       filelist = walkSync(path.join(dir, file), filelist);
-    }
-    else {
-      if(getFileExtension(file) === originFileExtension || getFileExtension(file) === newFileExtension){
-        var renamed = file.slice(0, -3) + '.ts';
-        fs.rename(path.join(dir, file), path.join(dir, renamed), function(err){
-          err ? console.log(err) : null;
-          
-        });
-        path.join(dir, renamed)
+    } else {
+      if (
+        getFileExtension(file) === originFileExtension ||
+        getFileExtension(file) === newFileExtension
+      ) {
+        var renamed = file.slice(0, -3) + `.${newFileExtension}`;
+        fs.rename(
+          path.join(dir, file),
+          path.join(dir, renamed),
+          function (err) {
+            err ? console.log(err) : null;
+          }
+        );
+        path.join(dir, renamed);
       }
     }
   });
   return filelist;
 };
 
-var getFileExtension = function(file){
+var getFileExtension = function (file) {
   var splitted = file.split('.');
   return splitted[splitted.length - 1];
 };
 
 try {
   walkSync(process.argv[2]);
-  console.log(ANSI_GREEN, 'finished renaming js files to ts', ANSI_DEFAULT);
-} catch(err){
-  console.log(ANSI_RED, 'something went wrong wile renaming js files', ANSI_DEFAULT);
+  console.log(
+    ANSI_GREEN,
+    `finished renaming ${originFileExtension} files to ${newFileExtension}`,
+    ANSI_DEFAULT
+  );
+} catch (err) {
+  console.log(
+    ANSI_RED,
+    'something went wrong wile renaming js files',
+    ANSI_DEFAULT
+  );
 }
